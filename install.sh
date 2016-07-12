33a3194bf7c710d58a46f0f8e4890ec399ce705f
Konstantin Delchev <kdelchev@gmail.com>
2016-07-12 21:09:39 +0300

    Add scripts

---
+++ install.sh
@@ -0,0 +1,3 @@
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+brew install google-chrome qt iterm2 the_silver_searcher pgadmin3 rabbitmq redis elasticsearch vim rbenv ruby-build openssl
