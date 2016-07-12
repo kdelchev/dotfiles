c989797d83f6659e0e394e63fb29df43d1cfcfc6
Konstantin Delchev <kdelchev@gmail.com>
2016-07-12 22:35:10 +0300

    Add tig

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
-brew install qt the_silver_searcher rabbitmq redis elasticsearch vim rbenv ruby-build openssl
+brew install qt the_silver_searcher rabbitmq redis elasticsearch vim rbenv ruby-build openssl tig
 
 brew cask install caffeine google-chrome iterm2 pgadmin3 atom
